e7c80fc4e44258a4055334bd76d298de8ef813bd
Guilherme Pimenta <pimenta14@gmail.com>
2013-07-05 15:16:05 -0300

    Primeiro Commit

---
+++ USPeriodico/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using USPeriodico.Models;
+using System.Data.Entity;
+using System.Web.Security;
+
+
+namespace USPeriodico.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Usuarios model)
+        {
+            if (ModelState.IsValid)
+            {
+                string username = model.username;
+                string password = model.password;
+
+                UsuariosEntities entities = new UsuariosEntities();
+
+                bool userValid = entities.Usuarios.Any(Usuarios => Usuarios.username == username);
+
+                if (userValid)
+                {
+                    FormsAuthentication.SetAuthCookie(username, false);
+                    return Redirect("/Home/IndexSafe");
+                }
+            }
+            return Redirect("/Home/");
+        }
+
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+
+            return Redirect("/Home/");
+        }
+
+    }
+}
