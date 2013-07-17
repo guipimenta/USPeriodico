414108559df69600ba4c5c969337a28b8b6c4868
Cassius Puodzius <puodzius@gmail.com>
2013-07-17 22:27:54 +0200

    Adicionada a página que lista usuários
    A página lista apenas o Id e o role do usuário. Quando forem adicionados
    os forms de aluno e empresa após o sign up, poderemos substituir o Id pelo
    nome (que é mais esclarecedor). Além disso ainda falta restringir a página
    para acesso apenas do Administrador.
    
    TODO:
    1. Substituir Id pelo nome do usuário
    2. Restringir acesso à página apenas para o Administrador

---
+++ USPeriodico/Controllers/AdministradorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using USPeriodico.Models;
+
+namespace USPeriodico.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        //
+        // GET: /Administrador/
+        public ActionResult ListarUsuarios()
+        {
+            usperiodicoEntities entities = new usperiodicoEntities();
+            IQueryable<Usuarios> listaOrdenada = entities.Usuarios.OrderBy(usuario => usuario.role).ThenBy(usuario => usuario.Id);
+            ViewBag.usuarios = listaOrdenada;
+            return View();
+        }
+
+    }
+}
