e85d7f1d82a4a079d2a335c373a40ff181604bca
Johnny <ultimos.jn@gmail.com>
2013-07-29 15:34:04 -0300

    autenticação teste

--- USPeriodico/Controllers/AdministradorController.cs
+++ USPeriodico/Controllers/AdministradorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using USPeriodico.Models;
+using System.Web.Security;
 
 namespace USPeriodico.Controllers
 {
@@ -13,7 +14,17 @@ namespace USPeriodico.Controllers
         // GET: /Administrador/
         public ActionResult ListarUsuarios()
         {
+            //verifica se é um usuário logado
+            if(HttpContext.User.Identity.Name.Equals(""))
+                return Redirect(FormsAuthentication.LoginUrl);
+            System.Web.Security.Roles.IsUserInRole("Administrador");
             usperiodicoEntities entities = new usperiodicoEntities();
+            
+            //verifica se o usuário é um administrador
+            bool userAdm = entities.Usuarios.Any(Usuarios => Usuarios.email == HttpContext.User.Identity.Name && Usuarios.role == 1);
+            if (!userAdm)
+                return Redirect("/Home/IndexSafe");
+
             IQueryable<Usuarios> listaOrdenada = entities.Usuarios.OrderBy(usuario => usuario.role).ThenBy(usuario => usuario.Id);
             ViewBag.usuarios = listaOrdenada;
             return View();
