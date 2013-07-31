23bc6a5595bc1dcb9ebd3b6758be35acac2c12ec
Cassius Puodzius <puodzius@gmail.com>
2013-07-31 13:31:23 +0200

    Adicionado controller de sincronizacao de sistemas
    A sincronizacao de cada tipo de evento ocorre em diferentes Actions do
    controller, mas nao esta completa ainda. Falta passar os parametros de
    maneira coerente com o combinado em sala, alem filtrar os eventos que
    tiveram alguma modificacao apos uma data (a ser passada no GET).

---
+++ USPeriodico/Controllers/SincronizadorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using USPeriodico.Models;
+
+namespace USPeriodico.Controllers
+{
+    public class SincronizadorController : Controller
+    {
+        //
+        // GET: /Sincronizador/EventoCEPE
+        [HttpGet]
+        public ActionResult EventoCEPE()
+        {
+            eventoCEPEEntities entities = new eventoCEPEEntities();
+            EventoCEPE[] eventos = entities.EventoCEPE.ToArray();
+            return Json(eventos, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: /Sincronizador/Estagio
+        [HttpGet]
+        public ActionResult Estagio()
+        {
+            estagioEntities entities = new estagioEntities();
+            Estagio[] eventos = entities.Estagio.ToArray();
+            return Json(eventos, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
