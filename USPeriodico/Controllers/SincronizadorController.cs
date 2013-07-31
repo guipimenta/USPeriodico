d6db70084ff8d85a8531b27cb6c0772cc1dec6dd
Cassius Puodzius <puodzius@gmail.com>
2013-08-01 01:30:22 +0200

    Adaptacao das classes para o Json acordado em sala
    O protocolo combinado em sala exigia certa padronizacao entre os eventos.
    Esta padronizacao ja esta sendo seguida, mas precisa ser finalizada (os
    comentarios no codigo indicam o que deve ser ainda alterado). Alem disso,
    a listagem dos eventos foi implementada.

--- USPeriodico/Controllers/SincronizadorController.cs
+++ USPeriodico/Controllers/SincronizadorController.cs
@@ -12,21 +12,108 @@ namespace USPeriodico.Controllers
         //
         // GET: /Sincronizador/EventoCEPE
         [HttpGet]
-        public ActionResult EventoCEPE()
+        public ActionResult EventoCEPE(string data)
         {
-            eventoCEPEEntities entities = new eventoCEPEEntities();
-            EventoCEPE[] eventos = entities.EventoCEPE.ToArray();
-            return Json(eventos, JsonRequestBehavior.AllowGet);
+            // Se o parametro data nao puder ser codificado, retornar um Json vazio
+            DateTime dataModificacao;
+            if (!DateTime.TryParse(data, out dataModificacao))
+            {
+                JsonEvento eventoCEPE_vazio = new JsonEvento(); // Usando JsonEvento para facilitar Debugging (pela diferenca de campos entre JsonEvento e JsonEventoCEPE)
+                return Json(eventoCEPE_vazio, JsonRequestBehavior.AllowGet);
+            }
+
+            eventoCEPEEntities eventoCEPE_entities = new eventoCEPEEntities();
+            EventoCEPE[] eventosCEPE = eventoCEPE_entities.EventoCEPE.ToArray();
+
+            alunoEntities aluno_entities = new alunoEntities();
+
+            Queue<JsonEventoCEPE> json_eventosCEPE = new Queue<JsonEventoCEPE>();
+            foreach (EventoCEPE eventoCEPE in eventosCEPE)
+            {
+                JsonEventoCEPE json_eventoCEPE = new JsonEventoCEPE();
+                json_eventoCEPE.id = eventoCEPE.ID;
+                json_eventoCEPE.tipo = "cepe";
+                json_eventoCEPE.nome = eventoCEPE.Nome;
+                // falta parametro de valido
+                json_eventoCEPE.descricao = eventoCEPE.Descricao;
+                // falta parametro de image_link
+                // falta parametro de data de atualizacao -> extremamente necessario para terminar esse controler devido ao filtro
+                json_eventoCEPE.nome_aluno = aluno_entities.Aluno.Find(eventoCEPE.AlunoID).Nome;
+                json_eventoCEPE.local = eventoCEPE.Local;
+                json_eventoCEPE.data = eventoCEPE.Data;
+                json_eventoCEPE.horario = eventoCEPE.Horario;
+                json_eventosCEPE.Enqueue(json_eventoCEPE);
+            }
+
+            return Json(json_eventosCEPE, JsonRequestBehavior.AllowGet);
         }
 
         // GET: /Sincronizador/Estagio
         [HttpGet]
-        public ActionResult Estagio()
+        public ActionResult Estagio(string data)
         {
+            // Se o parametro data nao puder ser codificado, retornar um Json vazio
+            DateTime dataModificacao;
+            if (!DateTime.TryParse(data, out dataModificacao))
+            {
+                JsonEvento estagio_vazio = new JsonEvento();
+                return Json(estagio_vazio, JsonRequestBehavior.AllowGet);
+            }
+
             estagioEntities entities = new estagioEntities();
-            Estagio[] eventos = entities.Estagio.ToArray();
-            return Json(eventos, JsonRequestBehavior.AllowGet);
+            Estagio[] estagios = entities.Estagio.ToArray();
+
+            Queue<JsonEstagio> json_estagios = new Queue<JsonEstagio>();
+            foreach (Estagio estagio in estagios)
+            {
+                JsonEstagio json_estagio = new JsonEstagio();
+                json_estagio.id = estagio.ID;
+                json_estagio.tipo = "estagio";
+                json_estagio.nome = estagio.BreveDescricao;
+                // falta parametro de valido
+                json_estagio.descricao = estagio.Descricao;
+                // falta parametro de image_link
+                // falta parametro de data de atualizacao -> extremamente necessario para terminar esse controler devido ao filtro
+                // falta Model da empresa, para pegar o nome dela a partir do ID
+                json_estagio.data_inicio = estagio.DataInicio;
+                json_estagio.duracao = estagio.Duracao.Value;
+                json_estagio.bolsa = Convert.ToDecimal(estagio.Bolsa); // trocar tipo de dado do estagio.Bolsa para Money
+                // falta Model da area, para pegar o nome dela a partir do ID
+                json_estagios.Enqueue(json_estagio);
+            }
+
+            return Json(json_estagios, JsonRequestBehavior.AllowGet);
         }
 
+    }
+
+    public class JsonEvento
+    {
+        public int id;
+        public string tipo;
+        public string nome;
+        public bool valido;
+        public string descricao;
+        public string image_link;
+        public DateTime data_atualizacao;
+    }
+
+    public class JsonEstagio : JsonEvento
+    {
+        public string nome_empresa;
+        public string descricao_empresa;
+        public DateTime data_inicio;
+        public int duracao;
+        public decimal bolsa;
+        public string area;
+    }
+
+    public class JsonEventoCEPE : JsonEvento
+    {
+        public string nome_aluno;
+        public string local;
+        public string esporte;
+        public DateTime data;
+        public TimeSpan horario;
     }
 }
