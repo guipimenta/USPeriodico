11ac7be2372241491593a0038a871fbe525d9686
Eduardo Almeida <eduardoolialmeida@gmail.com>
2013-08-06 08:58:10 -0300

    Update

--- USPeriodico/Controllers/HomeController.cs
+++ USPeriodico/Controllers/HomeController.cs
@@ -81,10 +81,63 @@ namespace USPeriodico.Controllers
         }
 
         [Authorize]
-        public ActionResult IndexSafe()
+        public ActionResult IndexSafe(int? mesDesejado = null)
         {
             if (Utilitarios.VerificaUsuario(1, HttpContext.User.Identity.Name) < 0)
                 return Redirect(FormsAuthentication.LoginUrl);
+
+            if (!mesDesejado.HasValue)
+            {
+                mesDesejado = DateTime.Now.Month;
+            }
+
+            eventosCEPE = entities.EventoCEPE.ToList();
+            estagios = entities2.Estagio.ToList();
+            int[] eventoID = new int[eventosCEPE.Count + estagios.Count];
+            int[] eventoTipo = new int[eventosCEPE.Count + estagios.Count];
+            String[] eventosNome = new String[eventosCEPE.Count + estagios.Count];
+            String[] eventosDataString = new String[eventosCEPE.Count + estagios.Count];
+            DateTime[] eventosData = new DateTime[eventosCEPE.Count + estagios.Count];
+            int i = 0;
+
+            if (mesDesejado < 1 || mesDesejado > 12)
+            {
+                mesDesejado = DateTime.Now.Month;
+            }
+
+
+            foreach (EventoCEPE evento in eventosCEPE)
+            {
+                if (evento.Data.Month == mesDesejado)
+                {
+                    eventoID[i] = evento.ID;
+                    eventosNome[i] = evento.Nome;
+                    eventosData[i] = evento.Data.Date;
+                    eventosDataString[i] = "" + evento.Data.Day + "/" + evento.Data.Month;
+                    eventoTipo[i] = 1;
+                }
+                i++;
+            }
+
+            foreach (Estagio estagio in estagios)
+            {
+                if (estagio.DataInicio.Month == mesDesejado)
+                {
+                    eventoID[i] = estagio.ID;
+                    eventosNome[i] = estagio.BreveDescricao;
+                    eventosData[i] = estagio.DataInicio.Date;
+                    eventosDataString[i] = "" + estagio.DataInicio.Day + "/" + estagio.DataInicio.Month;
+                    eventoTipo[i] = 2;
+                }
+                i++;
+            }
+
+            ViewBag.eventosID = eventoID;
+            ViewBag.eventosNome = eventosNome;
+            ViewBag.eventoTipo = eventoTipo;
+            ViewBag.eventosData = eventosData;
+            ViewBag.eventosDataString = eventosDataString;
+            ViewBag.eventoSize = i;
 
             return View();
         }
